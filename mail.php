b487b3804412d407cc06646797ebf8c9be58646a
Sajaldeb25 <sajaldebnath02@gmail.com>
2018-08-15 11:50:55 +0600

    Add files via upload
    mail.php and mews2.php uploded

---
+++ mail.php
@@ -0,0 +1,5 @@
+<?php
+	
+	mail('sajaldebnath02@gmail.com','sample mail','sample content','From: t.cse.bu.1997.19.09@gmail.com');
+
+?>
---
+++ news2.php
@@ -0,0 +1,330 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Home page</title>
+
+    <!-- Bootstrap -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+
+	<link href="assets/css/mdb.min.css" rel="stylesheet">
+	
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="assets/css/style.css" rel="stylesheet">
+	<link href="assets/css/news.css" rel="stylesheet">
+
+  </head>
+  <body>
+
+    
+    <div class="container-fluid">
+        <h1 style="text-align: center; font-weight: 700; font-size: 60px; ">Online Voting System</h1> <br>
+</div>
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-black sticky-top">
+    <a class="navbar-brand"><img src="logo.jpg" style="height: 43px; width: 70px;" ></a>
+   <ul class="navbar-nav " style="padding-left: 1040px">
+
+  </ul>
+</nav>
+
+<!--Start of News part-->
+
+	<div style="padding-left:200px; padding-top:20px; font-size:20px; height:30px; width:600px; "> </div>
+
+	<div class="news shadow" style="height:3000px;">
+		<div style=" margin: 30px; width: 80% ; text-align:left ; padding-top: 18px; font-size:1em;">
+			Hosted By <span style="color: rgb(89, 206, 26) ;">Admin</span> on June 29th, 2018.
+			<p style=" margin: 15px; color: rgb(119,119,119) ; font-size: 1em ; font-style:italic ;"> Last Updated on June 28th, 2018.</p>
+			
+		</div>
+
+	<h3>Teachers Association election BU, 2018</h3><hr>
+	<P style=" margin: 30px; font-size:17px; text-align:justify;">Hey everyone, Hope you khow about <b>The Teachers association</b> of Barisal University. The association member election was held on 15th July, 2018 to elect 17 of 30 members to the Association, with the result declared on that day.  The scheduled election in Williamnagar constituency 
+	  was delayed to an undetermined date following the death of Nationalist Congress Party candidate Jonathone Sangma in an IED blast in East Garo Hills district on February 18, 2018. The incumbent Indian National Congress government, led by Chief Minister Mukul Sangma
+	  attempted to win a re-election for the third time in a row. Here is the summary:</p>
+        <h4 style="text-align:center">Summary of vote result</h4>
+	    <p  style="padding-left: 40px; padding-top:30px; color: green; !important">In President Position:</p>
+	  
+	    <div style="padding-left: 140px;!important">
+			<table class="table table-borderless">
+				<thead>
+					<tr>
+					  <th scope="col" style="color:rgb(0, 123, 255); !important ">Candidates Name</th>
+					  <th scope="col" style="color:rgb(0, 123, 255); !important ">Number of vote</th>
+					</tr>
+				</thead>
+			  
+				<tbody>
+					<tr>
+				 
+					<td>Mark H.J. saj</td>
+					<td>30</td>
+			
+					</tr>
+					<tr>
+					  <td>Taluk H. Otto</td>
+					  <td>11</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		
+		
+		 <p  style="padding-left: 40px; padding-top:30px; color: green; !important">In Secretary Position:</p>
+	  
+	    <div style="padding-left: 140px;!important">
+			<table class="table table-borderless">
+				<thead>
+					<tr>
+					  <th scope="col" style="color:rgb(0, 123, 255); !important ">Candidates Name</th>
+					  <th scope="col" style="color:rgb(0, 123, 255); !important ">Number of vote</th>
+					</tr>
+				</thead>
+			  
+				<tbody>
+					<tr>
+				 
+					<td>Sniawbhalang Dhar</td>
+					<td>30</td>
+			
+					</tr>
+					<tr>
+					  <td>Wailadmiki Shylla</td>
+					  <td>25</td>
+					</tr>
+					
+					<tr>
+					  <td>Comingone Ymbon</td>
+					  <td>19</td>
+					</tr>
+					
+				
+					<tr>
+					  <td>Nujorki Sungoh</td>
+					  <td>19</td>
+					</tr>
+					
+					
+				</tbody>
+			</table>
+		</div>
+	
+
+
+		<p  style="padding-left: 40px; padding-top:30px; color: green; !important">In Treasure Position:</p>
+	  
+	    <div style="padding-left: 140px;!important">
+			<table class="table table-borderless">
+				<thead>
+					<tr>
+					  <th scope="col" style="color:rgb(0, 123, 255); !important ">Candidates Name</th>
+					  <th scope="col" style="color:rgb(0, 123, 255); !important ">Number of vote</th>
+					</tr>
+				</thead>
+			  
+				<tbody>
+					<tr>
+				 
+					<td>Kyrmen Shylla</td>
+					<td>30</td>
+			
+					</tr>
+					<tr>
+					  <td>Lahkmen Rymbui</td>
+					  <td>25</td>
+					</tr>
+					
+					</tr>
+					<tr>
+					  <td>Dasakhiatbha Lamare</td>
+					  <td>19</td>
+					</tr>
+					
+					
+				</tbody>
+			</table>
+		</div>
+	
+	
+	<p  style="padding-left: 40px; padding-top:30px; color: green; !important">In Join-secretary Position:</p>
+	  
+	    <div style="padding-left: 140px;!important">
+			<table class="table table-borderless">
+				<thead>
+					<tr>
+					  <th scope="col" style="color:rgb(0, 123, 255); !important ">Candidates Name</th>
+					  <th scope="col" style="color:rgb(0, 123, 255); !important ">Number of vote</th>
+					</tr>
+				</thead>
+			  
+				<tbody>
+					
+					<tr>
+					  <td> 	Jason Sawkmie Mawlong</td>
+					  <td>40</td>
+					</tr>
+					<tr>
+					  <td> 	David A Nongrum</td>
+					  <td>25</td>
+					</tr>
+					
+				</tbody>
+			</table>
+		</div>
+		
+		<p  style="padding-left: 40px; padding-top:30px; color: green; !important">In Vice-president Position:</p>
+	  
+	    <div style="padding-left: 140px;!important">
+			<table class="table table-borderless">
+				<thead>
+					<tr>
+					  <th scope="col" style="color:rgb(0, 123, 255); !important ">Candidates Name</th>
+					  <th scope="col" style="color:rgb(0, 123, 255); !important ">Number of vote</th>
+					</tr>
+				</thead>
+			  
+				<tbody>
+					
+					<tr>
+					  <td> Alexander Laloo Hek</td>
+					  <td>34</td>
+					</tr>
+					
+					<tr>
+					  <td>Process T. Sawkmie</td>
+					  <td>32</td>
+					</tr>
+					
+				</tbody>
+			</table>
+		</div>
+		
+		
+		<p  style="padding-left: 40px; padding-top:30px; color: green; !important">In Executive-members Position:</p>
+	  
+	    <div style="padding-left: 140px;!important">
+			<table class="table table-borderless">
+				<thead>
+					<tr>
+					  <th scope="col" style="color:rgb(0, 123, 255); !important ">Candidates Name</th>
+					  <th scope="col" style="color:rgb(0, 123, 255); !important ">Number of vote</th>
+					</tr>
+				</thead>
+			  
+				<tbody>
+					<tr>
+					  <td> Mayralborn Syiem</td>
+					  <td>19</td>
+					</tr>
+					
+					<tr>
+					  <td>Sosthenes Sohtun</td>
+					  <td>11</td>
+					</tr>
+					
+					<tr>
+					  <td> 	Ampareen Lyngdoh</td>
+					  <td>11</td>
+					</tr>
+					
+					<tr>
+					  <td>Shitlang Pale</td>
+					  <td>11</td>
+					</tr>
+					
+					<tr>
+					  <td>Adelbert Nongrum</td>
+					  <td>11</td>
+					</tr>
+					
+					<tr>
+					  <td>Shitlang Pale</td>
+					  <td>11</td>
+					</tr>
+					
+				</tbody>
+			</table>
+		</div>
+		
+		
+		
+		<p  style="padding-left: 270px; font-size:1.5rem; padding-top:30px; color: rgb(255, 187, 51); !important">All Elected Members of the election:</p>
+	  
+	    <div style="padding-left: 140px;!important">
+			<table class="table table-borderless">
+				<thead>
+					<tr>
+					  <th scope="col" style="color:rgb(0, 123, 255); font-size:1.1rem; !important ">Candidates Name</th>
+					  <th scope="col" style="color:rgb(0, 123, 255);font-size:1.1rem; !important ">Post name</th>
+					</tr>
+				</thead>
+			  
+				<tbody>
+					<tr>
+					  <td>Mark H.J. saj</td>
+					  <td>President</td>
+					</tr>
+					
+					<tr>
+					  <td>Sniawbhalang Dhar</td>
+					  <td>Secretary</td>
+					</tr>
+					
+					<tr>
+					  <td>Kyrmen Shylla</td>
+					  <td>Tresurer</td>
+					</tr>
+					
+					<tr>
+					  <td> 	Jason Sawkmie Mawlong</td>
+					  <td>Join-secretary</td>
+					</tr>
+					
+					<tr>
+					  <td>Alexander Laloo Hek</td>
+					  <td>Vice-president</td>
+					</tr>
+					
+					<tr>
+					  <td> Mayralborn Syiem</td>
+					  <td>Executive Member</td>
+					</tr>
+					
+					<tr>
+					  <td> Malahar Syiem</td>
+					  <td>Executive Member</td>
+					</tr>
+					
+				</tbody>
+			</table>
+		</div>
+	
+		
+	  
+	</div>
+
+
+
+
+<!--End of News part-->
+
+
+    <!-- Footer -->
+		<footer class="page-footer font-small black" style="background-color:#0b0c0c; !important">
+
+		  <!-- Copyright -->
+		  <div class="footer-copyright text-center py-3">© 2018 Copyright:
+			<a href="">Teamtwenty5</a>
+		  </div>
+		  <!-- Copyright -->
+
+		</footer>
+		<!-- Footer -->
+  
+    
+  </body>
+</html>
