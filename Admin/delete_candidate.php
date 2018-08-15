9fbf063605aeefe7d4af4605ff89d4182a3617d7
Sajaldeb25 <sajaldebnath02@gmail.com>
2018-08-15 11:38:32 +0600

    Update delete_candidate.php
    Updated

--- Admin/delete_candidate.php
+++ Admin/delete_candidate.php
@@ -3,10 +3,14 @@
 	session_start();
 	require('connection.php');
 	
+	if(empty($_SESSION['admin_id'])){
+		header("location:index.php");
+	}
+	
 	$id = $_GET['id'];
 		 
 		 
-	$result = mysqli_query( $link," DELETE FROM tbcandidates WHERE candidate_id='$id' " )
+	$result = mysqli_query( $link," DELETE FROM candidate WHERE c_id='$id' " )
 	 or die("The position does not exist ... \n"); 
 
 	header("Location: manage-candidate.php");
