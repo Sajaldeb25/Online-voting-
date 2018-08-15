5babd2f0991f623d3c9094bc171fdc778b8123cd
Sajaldeb25 <sajaldebnath02@gmail.com>
2018-08-15 11:40:48 +0600

    Update voterdelete.php

--- Admin/voterdelete.php
+++ Admin/voterdelete.php
@@ -3,10 +3,14 @@
 	session_start();
 	require('connection.php');
 	
+	if(empty($_SESSION['admin_id'])){
+		header("location:index.php");
+	}
+	
 	$id = $_GET['id'];
 		 
 		 
-	$result = mysqli_query( $link," DELETE FROM tbmembers WHERE member_id='$id' " )
+	$result = mysqli_query( $link," DELETE FROM voter WHERE id='$id' " )
 	 or die("The position does not exist ... \n"); 
 
 	header("Location: manage-voters.php");
